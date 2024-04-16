dca3acce0a589798daa50fcd6db69f4265d34224
himanshuguptadb <123580084+himanshuguptadb@users.noreply.github.com>
2024-04-16 09:37:28 -0400

    Update job_reference_table.sql

--- job_reference_table.sql
+++ job_reference_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `job_reference_table`
 (
-  share_name STRING,
-  date_loaded TIMESTAMP,
-  status STRING,
-  version_number INT64
+  share_name STRING, -- name of the share sharename.schema.table
+  date_loaded TIMESTAMP, -- Date timestamp when the dataproc job loaded data into the bigquery table
+  status STRING, -- status of the job
+  version_number INT64 -- latest version of delta table processed
 );
